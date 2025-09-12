250cbdaef8024893b2390ea4a16d98af8e7a233a
faridkinyobi <faridkin7@gmail.com>
2025-09-12 11:07:23 +0700

    update index components dashboard organisms

--- src/components/dashboard/organisms/TableOrganisms/index.ts
+++ src/components/dashboard/organisms/TableOrganisms/index.ts
@@ -2,5 +2,7 @@ export { default as TablelMasterInventory } from "./TablelMasterInventory";
 export { default as TablelMasterMitra } from "./TablelMasterMitra";
 export { default as TablelBlanjaMitra } from "./TablelBlanjaMitra";
 export { default as TablelTransaksiMitra } from "./TablelTransaksiMitra";
+export { default as TablelLaporanInventory } from "./TablelLaporanInventory";
+export { default as TablelLaporanPenjualan } from "./TablelLaporanPenjualan";
 
 // export * from "./TableIInventoryOutlite";
