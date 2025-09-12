f783acb62577d83f8c04e63b2646cc04af0ddf1f
faridkinyobi <faridkin7@gmail.com>
2025-09-12 10:49:19 +0700

    update component dashboard organisms top table header

--- src/components/dashboard/organisms/TopTableHeader.tsx
+++ src/components/dashboard/organisms/TopTableHeader.tsx
@@ -7,31 +7,65 @@ import { Label } from "@/components/ui/label";
 
 import TableFilter from "../atoms/TableFilter";
 import { useQueryParamStore } from "@/stores";
+import { ShoppingCart } from "lucide-react";
+import { useBlanjaStore } from "@/stores/useBlanjaStore";
+import { Badge } from "@/components/ui/badge";
 
 interface ItopHaederProps {
 	isOpenLaporan?: boolean;
+	isOpenLaporanPenjualan?: boolean
 	total: number;
 	handleButtonAdd?: () => void;
+	handleButtonShopping?: () => void;
 }
 export default function TopTableHeader({
 	isOpenLaporan,
-	total,
+	total = 0,
 	handleButtonAdd,
+	handleButtonShopping,
+	isOpenLaporanPenjualan
 }: ItopHaederProps) {
 	const { setQueryParamsStore, endDate, startDate } = useQueryParamStore();
+	const { items } = useBlanjaStore();
 
 	return (
 		<div className="space-y-2">
 			<div className="py-5 border-b bg-transparent border-b-border-secondary">
 				<p className="px-3">Total Data: {total}</p>
 			</div>
-			<div className="py-4 px-3 space-y-3 bg-background">
+
+			<div className={`flex flex-wrap justify-between  gap-2 py-4 px-3 bg-background ${!isOpenLaporanPenjualan ? "items-center" : "items-end"}`}>
+				<TableFilter />
+
+				{/* ADD end Shopping keranjang */}
+				<div className="flex gap-2">
+					{handleButtonAdd && (
+						<Button className="cursor-pointer" onClick={handleButtonAdd}>
+							Add Data
+						</Button>
+					)}
+					{handleButtonShopping && (
+						<div className="relative">
+							<Button
+								onClick={handleButtonShopping}
+								className="cursor-pointer"
+							>
+								<Badge className="absolute bg-[#109900] text-center -top-2 -left-2 h-6 max-w-6 rounded-full px-2 -inset-2.5 text-base">
+									{items.length}
+								</Badge>
+
+								<ShoppingCart className="w-4 h-4" />
+							</Button>
+						</div>
+					)}
+				</div>
+				{/* Start & end Date */}
 				{isOpenLaporan && (
 					<div className="flex gap-3">
 						<div className="space-y-3">
-							<Label>Tanggal Mulai</Label>
+							<Label htmlFor="StartDate">Tanggal Mulai</Label>
 							<Input
-								placeholder="Search"
+								id="startDate"
 								type="date"
 								value={startDate}
 								onChange={(e) =>
@@ -40,9 +74,9 @@ export default function TopTableHeader({
 							/>
 						</div>
 						<div className="space-y-3">
-							<Label>Tanggal Akhir</Label>
+							<Label htmlFor="endDate">Tanggal Akhir</Label>
 							<Input
-								placeholder="Search"
+								id="endDate"
 								type="date"
 								value={endDate}
 								onChange={(e) =>
@@ -52,32 +86,26 @@ export default function TopTableHeader({
 						</div>
 					</div>
 				)}
-				<div className="flex flex-wrap justify-between gap-2">
-					<TableFilter />
-					{handleButtonAdd && (
-						<Button className="cursor-pointer" onClick={handleButtonAdd}>
-							Add Data
+
+				{/* button Laporan penjualan end laporan belanja */}
+				{isOpenLaporan && isOpenLaporanPenjualan && (
+					<div className="flex">
+						<Button
+							className="rounded-none data-[active=true]:text-primary data-[active=true]:bg-accent data-[active=true]:border-b data-[active=true]:border-primary  "
+							variant={"ghost"}
+							isActive={true}
+						>
+							Penjualan Menu
 						</Button>
-					)}
-					{isOpenLaporan && (
-						<div className="flex">
-							<Button
-								className="cursor-pointer rounded-none data-[active=true]:text-primary data-[active=true]:bg-accent data-[active=true]:border-b data-[active=true]:border-primary  "
-								variant={"ghost"}
-								isActive={true}
-							>
-								Penjualan Menu
-							</Button>
-							<Button
-								className="cursor-pointer rounded-none data-[active=false]:text-primary/70 data-[active=false]:border-b data-[active=false]:border-primary/70"
-								variant={"ghost"}
-							>
-								Belanja mitra
-							</Button>
-						</div>
-					)}
-				</div>
+						<Button
+							className="rounded-none data-[active=false]:text-primary/70 data-[active=false]:border-b data-[active=false]:border-primary/70"
+							variant={"ghost"}
+						>
+							Belanja mitra
+						</Button>
+					</div>
+				)}
 			</div>
-		</div>
+		</div >
 	);
 }
