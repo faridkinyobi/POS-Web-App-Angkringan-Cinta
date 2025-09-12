32327268e4ee3c09eb811929c25e99e782825137
faridkinyobi <faridkin7@gmail.com>
2025-09-12 10:54:15 +0700

    update condition sheet shopping mitra

--- src/components/dashboard/organisms/SheetBelanjaMitra.tsx
+++ src/components/dashboard/organisms/SheetBelanjaMitra.tsx
@@ -4,12 +4,14 @@ import { CartSummary } from "../atoms/CartSummary";
 import { useBlanjaStore, usePlaceOrder } from "@/stores/useBlanjaStore";
 import { OrderForm } from "./FormOrganisms/FormBlanja";
 
-export default function SheetBelanjaMitra() {
+export default function SheetBelanjaMitra(ishidden: { ishidden?: boolean }) {
 	const { isNext } = usePlaceOrder();
 	const { grandTotal, subTotal } = useBlanjaStore();
 
 	return (
-		<div className="p-4 w-[265px] sticky bg-background-secondary rounded-lg flex flex-col justify-between">
+		<div
+			className={`${ishidden.ishidden ? "hidden" : "block"} p-4 w-[265px] sticky bg-background-secondary rounded-lg flex flex-col justify-between`}
+		>
 			<div className="w-full space-y-4">
 				<h3 className="md:text-3xl font-semibold">Card</h3>
 				{!isNext ? <BelanjaMitralist /> : <OrderForm />}
